308b3102ce6616d4585e52d15e64d33d69f24c1e
raviteja-g <31245002+raviteja-g@users.noreply.github.com>
2025-04-08 16:50:46 +0530

    Update btp_dev_deploy.sh

--- .github/workflows/scripts/btp_dev_deploy.sh
+++ .github/workflows/scripts/btp_dev_deploy.sh
@@ -44,7 +44,7 @@ echo '############## nodeId ##############'
 echo $response | jq -r '.queueEntries[0].nodeId'
 nodeId=$(echo $response | jq -r '.queueEntries[0].nodeId')
 
-curl -v --location --request POST 'https://transport-service-app-backend.ts.cfapps.us10.hana.ondemand.com/v2/nodes/${nodeId}/transportRequests/import' --header 'Content-Type: application/json' --header "Authorization: Bearer $token" --data-raw '{ "namedUser": "raviteja.gattu@sap.com", "transportRequests": [{ '"$transportRequestId"' }]}'
+curl -v --location --request POST 'https://transport-service-app-backend.ts.cfapps.us10.hana.ondemand.com/v2/nodes/'"$nodeId"'/transportRequests/import' --header 'Content-Type: application/json' --header "Authorization: Bearer $token" --data-raw '{ "namedUser": "raviteja.gattu@sap.com", "transportRequests": [{ '"$transportRequestId"' }]}'
 echo '############## Importing Success  ##############'
 
 
